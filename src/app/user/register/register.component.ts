6defa1b26b7f9a55f467d05c37f7584f03e96836
AntonScherba <antonscherba95@gmail.com>
2022-09-08 12:15:10 +0200

    Update register.component.ts

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -26,7 +26,7 @@ export class RegisterComponent {
     ]);
     password = new FormControl('', [
       Validators.required,
-      Validators.pattern('^(?=.*d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'),
+      Validators.pattern('^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$'),
     ]);
     confirm_password = new FormControl('', [Validators.required]);
     phoneNumber = new FormControl('', [
